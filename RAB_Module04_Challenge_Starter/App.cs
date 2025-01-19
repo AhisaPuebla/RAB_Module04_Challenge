6ce892029c00ae17dcc8cf6d1fd8b497fcbcdaa7
Ahisa Puebla Souza <ahisapuebla@gmail.com>
2025-01-19 22:13:53 +0100

    Challenge 04

--- RAB_Module04_Challenge_Starter/App.cs
+++ RAB_Module04_Challenge_Starter/App.cs
@@ -5,6 +5,11 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace RAB_Module04_Challenge_Starter
 {
@@ -12,12 +17,144 @@ namespace RAB_Module04_Challenge_Starter
 	{
 		public Result OnStartup(UIControlledApplication app)
 		{
-			// write your ribbon code here
+			// 1. Create tab and panel 
+			string tabName1 = "Revit Add-in Bootcamp";
 
-			return Result.Succeeded;
+
+            try // - safe version
+            {
+				app.CreateRibbonTab(tabName1);
+			}
+			catch (Exception error)
+			{
+				Debug.Print("Tab already exists. Using existing panel.");
+				Debug.Print(error.Message);
+			}
+
+			// 2. Create panel
+			string PanelName1 = "Scavenger Hunt";
+			//RibbonPanel panel1 = app.CreateRibbonPanel(tabName, PanelName1);
+
+            RibbonPanel panel = CreateGetPanel(app, tabName1, PanelName1); // - safe method 
+
+			// 3. Create button data
+			PushButtonData buttonData1 = new PushButtonData(
+				"Tool 1",
+				"Command 1",
+				Assembly.GetExecutingAssembly().Location, 
+				"RAB_Module04_Challenge_Starter.cmd1"
+                );
+
+            PushButtonData buttonData2 = new PushButtonData(
+                "Tool 2",
+                "Command 2",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd2"
+                );
+
+            PushButtonData buttonData3 = new PushButtonData(
+                 "Tool 3",
+                 "Command 3",
+                 Assembly.GetExecutingAssembly().Location,
+                 "RAB_Module04_Challenge_Starter.cmd3"
+                 );
+
+            PushButtonData buttonData4 = new PushButtonData(
+                "Tool 4",
+                "Command 4",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd4"
+                );
+
+            PushButtonData buttonData5 = new PushButtonData(
+                "Tool 5",
+                "Command 5",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd5"
+                );
+
+            PushButtonData buttonData6 = new PushButtonData(
+                "Tool 6",
+                "Command 6",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd6"
+                );
+
+            PushButtonData buttonData7 = new PushButtonData(
+                "Tool 7",
+                "Command 7",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd7"
+                );
+
+            PushButtonData buttonData8 = new PushButtonData(
+                "Tool 8",
+                "Command 8",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd8"
+                );
+
+            PushButtonData buttonData9 = new PushButtonData(
+                "Tool 9",
+                "Command 9",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd9"
+                );
+
+            PushButtonData buttonData10 = new PushButtonData(
+                "Tool 10",
+                "Command 10",
+                Assembly.GetExecutingAssembly().Location,
+                "RAB_Module04_Challenge_Starter.cmd10"
+                );
+
+
+            //a - push buttons
+            PushButton button1 = panel.AddItem(buttonData1) as PushButton;
+            PushButton button2 = panel.AddItem(buttonData2) as PushButton;
+
+            //b - stacked row
+            panel.AddStackedItems(buttonData3, buttonData4, buttonData5);
+
+            //c - split button
+            SplitButtonData splitButtonData = new SplitButtonData("SplitButton", "Split\rButton");
+            SplitButton splitButton = panel.AddItem(splitButtonData) as SplitButton;
+            splitButton.AddPushButton(buttonData6);
+            splitButton.AddPushButton(buttonData7);
+
+            //d - pull down button
+            PulldownButtonData pulldownButtonData = new PulldownButtonData("More tools", "Pulldown\rButton");
+            //pulldownButtonData.LargeImage = ConvertToImageSource(Properties.Resources.Green_32);
+            PulldownButton pulldownButton = panel.AddItem(pulldownButtonData) as PulldownButton;
+            pulldownButton.AddPushButton(buttonData8);
+            pulldownButton.AddPushButton(buttonData9);
+            pulldownButton.AddPushButton(buttonData10);
+
+
+
+
+            return Result.Succeeded;
 		}
 
-		public Result OnShutdown(UIControlledApplication a)
+        private RibbonPanel CreateGetPanel(UIControlledApplication app, string tabName1, string panelName1)
+        {
+            //look for panel in tab
+            foreach (RibbonPanel panel in app.GetRibbonPanels(tabName1))
+            {
+                if (panel.Name == panelName1)
+                {
+                    return panel;
+                }
+
+            }
+            // panel not found, create it
+            //RibbonPanel returnPanel = app.CreateRibbonPanel(tabName1, panelName1);
+            //return returnPanel;
+
+            return app.CreateRibbonPanel(tabName1, panelName1);
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
 		{
 			return Result.Succeeded;
 		}
